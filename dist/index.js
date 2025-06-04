82efe8620a13e550e8c0e7c7a93d76aca8c1a76d
Andy Hsu <i@nn.ci>
2025-06-04 22:19:29 +0800

    feat: add support for Windows 7

--- dist/index.js
+++ dist/index.js
@@ -36634,7 +36634,70 @@ registerEngine({
     }
 });
 
+;// CONCATENATED MODULE: ./src/engines/win7.ts
+
+
+
+
+async function setupZcx(target) {
+    const zcc = `#!/bin/sh
+zig cc -target ${target} $@
+`;
+    const zcxx = `#!/bin/sh
+zig c++ -target ${target} $@
+`;
+    if (!external_fs_default().existsSync('/usr/local/bin')) {
+        external_fs_default().mkdirSync('/usr/local/bin', { recursive: true });
+    }
+    external_fs_default().writeFileSync('/usr/local/bin/zcc', zcc);
+    external_fs_default().writeFileSync('/usr/local/bin/z++', zcxx);
+    await $$ `chmod +x /usr/local/bin/zcc /usr/local/bin/z++`;
+}
+async function getGoVersion() {
+    const goVersion = await $ `go version`;
+    // go version go1.24.1 darwin/arm64
+    const match = goVersion.stdout.match(/go(\d+\.\d+\.\d+)/);
+    if (!match) {
+        throw new Error('Failed to get go version');
+    }
+    return match[1];
+}
+async function setupWin7Go() {
+    const goVersion = await getGoVersion();
+    await $$ `curl -L https://github.com/XTLS/go-win7/releases/download/patched-${goVersion}/go-for-win7-linux-amd64.zip -o go-win7.zip`;
+    await $$ `unzip go-win7.zip -d ~/go-win7`;
+    await $$ `rm go-win7.zip`;
+    return '~/go-win7/bin/go';
+}
+const zigTargetMap = {
+    'windows7-386': 'x86-windows-gnu',
+    'windows7-amd64': 'x86_64-windows-gnu'
+};
+registerEngine({
+    targets: ['windows7-386', 'windows7-amd64'],
+    async prepare(input) {
+        await $$ `sudo snap install zig --classic --beta`;
+        await setupWin7Go();
+    },
+    async run(input) {
+        const target = input.target;
+        const [os, arch] = target.split('-');
+        const zigTarget = zigTargetMap[target];
+        await setupZcx(zigTarget);
+        await input.$({
+            env: {
+                CGO_ENABLED: '1',
+                GOOS: 'windows',
+                GOARCH: arch,
+                CC: 'zcc',
+                CXX: 'z++'
+            }
+        }) `~/go-win7/bin/go build -o ${TempBinName}.exe ${calFlags(input.flags)} ${input.pkgs}`;
+    }
+});
+
 ;// CONCATENATED MODULE: ./src/engines/all.ts
+
 
 
 
