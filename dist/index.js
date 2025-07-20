69c4cbdcc25a94d6c9de91af0bc19f48b76ae1b7
Yinan Qin <39023210+elysia-best@users.noreply.github.com>
2025-07-20 13:49:41 +0000

    fix: escape double quotes in curl commands for LoongArch setup

--- dist/index.js
+++ dist/index.js
@@ -44494,13 +44494,13 @@ async function setupABI1_0Go() {
     // Get major and minor version
     const majorMinorVersion = goVersion.split('.')[0] + '.' + goVersion.split('.')[1];
     // https://ftp.loongnix.cn/toolchain/golang/go-1.24/abi1.0/go1.24.3.linux-amd64.tar.gz
-    await $$ `curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" -fsSL  https://ftp.loongnix.cn/toolchain/golang/go-${majorMinorVersion}/abi1.0/go${goVersion}.linux-amd64.tar.gz -o go-loong64-abi1.0.tar.gz`;
+    await $$ `curl -A \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -fsSL  https://ftp.loongnix.cn/toolchain/golang/go-${majorMinorVersion}/abi1.0/go${goVersion}.linux-amd64.tar.gz -o go-loong64-abi1.0.tar.gz`;
     await $$ `tar –xzf go-loong64-abi1.0.tar.gz -C go-loong64-abi1.0 --strip-components=1`;
     await $$ `rm go-loong64-abi1.0.tar.gz`;
     return `${loongarch_abi1_0_cwd}/go-loong64-abi1.0/bin/go`;
 }
 async function setupABI1_0GCC() {
-    await $$ `curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" -fsSL  https://ftp.loongnix.cn/toolchain/gcc/release/loongarch/gcc8/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.6.tar.xz -o gcc8-loong64-abi1.0.tar.gz`;
+    await $$ `curl -A \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -fsSL  https://ftp.loongnix.cn/toolchain/gcc/release/loongarch/gcc8/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.6.tar.xz -o gcc8-loong64-abi1.0.tar.gz`;
     await $$ `tar –xzf gcc8-loong64-abi1.0.tar.gz -C gcc8-loong64-abi1.0 --strip-components=1`;
     await $$ `rm gcc8-loong64-abi1.0.tar.gz`;
     return `${loongarch_abi1_0_cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-`;
