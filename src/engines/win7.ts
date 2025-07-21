d51d7ae526711042389db5310d6b3ade2715ba83
Yinan Qin <39023210+elysia-best@users.noreply.github.com>
2025-07-21 11:40:00 +0000

    fix(curl): order in setup scripts for engines

--- src/engines/win7.ts
+++ src/engines/win7.ts
@@ -32,7 +32,7 @@ async function getGoVersion() {
 
 async function setupWin7Go(input: Input) {
   const goVersion = await getGoVersion()
-  await $$`curl -fsSL ${String.raw`-H "Authorization: Bearer ${input.github_token}"`} --retry 3 https://github.com/XTLS/go-win7/releases/download/patched-${goVersion}/go-for-win7-linux-amd64.zip -o go-win7.zip`
+  await $$`curl -fsSL --retry 3 -o go-win7.zip https://github.com/XTLS/go-win7/releases/download/patched-${goVersion}/go-for-win7-linux-amd64.zip ${String.raw`-H 'Authorization: Bearer ${input.github_token}'`}`
   await $$`unzip go-win7.zip -d ${cwd}/go-win7`
   await $$`rm go-win7.zip`
   return `${cwd}/go-win7/bin/go`
