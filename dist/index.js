918a2a1ccbd3f48c7a2e8b1e972be112bcd1f642
Yinan Qin <39023210+elysia-best@users.noreply.github.com>
2025-07-24 12:29:34 +0000

    feat(loongarch64): enable cgo and clean up after build

--- dist/index.js
+++ dist/index.js
@@ -44414,6 +44414,15 @@ registerEngine({
                     CXX: `${loongarch64_cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-g++`
                 }
             }) `${loongarch64_cwd}/go-loong64-abi1.0/bin/go build -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`;
+            await input.$({
+                env: {
+                    CGO_ENABLED: '1',
+                    GOOS: os,
+                    GOARCH: arch,
+                    CC: `${loongarch64_cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-gcc`,
+                    CXX: `${loongarch64_cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-g++`
+                }
+            }) `${loongarch64_cwd}/go-loong64-abi1.0/bin/go clean`;
         }
         else {
             await input.$({
@@ -44425,6 +44434,15 @@ registerEngine({
                     CXX: `${loongarch64_cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-g++`
                 }
             }) `go build -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`;
+            await input.$({
+                env: {
+                    CGO_ENABLED: '1',
+                    GOOS: os,
+                    GOARCH: arch,
+                    CC: `${loongarch64_cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-gcc`,
+                    CXX: `${loongarch64_cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-g++`
+                }
+            }) `go clean`;
         }
     }
 });
