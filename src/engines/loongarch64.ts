dc43f52b2ff1c3ed621481ac67be693fe0d1c43d
Yinan Qin <39023210+elysia-best@users.noreply.github.com>
2025-07-24 12:47:56 +0000

    fix(loong64): force recompile different abi targets

--- src/engines/loongarch64.ts
+++ src/engines/loongarch64.ts
@@ -68,16 +68,7 @@ registerEngine({
           CC: `${cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-gcc`,
           CXX: `${cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-g++`
         }
-      })`${cwd}/go-loong64-abi1.0/bin/go build -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`
-      await input.$({
-        env: {
-          CGO_ENABLED: '1',
-          GOOS: os,
-          GOARCH: arch,
-          CC: `${cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-gcc`,
-          CXX: `${cwd}/gcc8-loong64-abi1.0/bin/loongarch64-linux-gnu-g++`
-        }
-      })`${cwd}/go-loong64-abi1.0/bin/go clean`
+      })`${cwd}/go-loong64-abi1.0/bin/go build -a -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`
     } else {
       await input.$({
         env: {
@@ -87,16 +78,7 @@ registerEngine({
           CC: `${cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-gcc`,
           CXX: `${cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-g++`
         }
-      })`go build -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`
-      await input.$({
-        env: {
-          CGO_ENABLED: '1',
-          GOOS: os,
-          GOARCH: arch,
-          CC: `${cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-gcc`,
-          CXX: `${cwd}/gcc12-loong64-abi2.0/bin/loongarch64-unknown-linux-gnu-g++`
-        }
-      })`go clean`
+      })`go build -a -o ${TempBinName} ${calFlags(input.flags)} ${input.pkgs}`
     }
   }
 })
